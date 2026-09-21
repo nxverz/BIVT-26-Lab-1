1b782cb16dbdecccab302bd70a56e18fa0681cb4
nxverz <dimaa.posnikov@mail.ru>
2026-09-21 14:46:22 +0300

    Update Program.cs

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -4,13 +4,15 @@
     {
         public static void Main()
         {
-            //White white = new White();
-            //Console.WriteLine(white.Task1(1.2));
-            //Console.WriteLine(white.Task2(2));
-            //Console.WriteLine(white.Task3(2.5, 1.89));
-            //Console.WriteLine(white.Task4(-2.5, 1.89));
-            //Console.WriteLine(white.Task5(0.5));
-            //Console.WriteLine(white.Task6(2.5, 1.89, 3));
+            Green green = new Green();
+            Console.WriteLine(green.Task1(-0.2));
+            Console.WriteLine(green.Task2(2, 3));
+            Console.WriteLine(green.Task3(2, 1));
+            Console.WriteLine(green.Task4(-2, 1, 3));
+            Console.WriteLine(green.Task5(0.5));
+            Console.WriteLine(green.Task6(2.5, 1.89));
+            Console.WriteLine(green.Task7(5));
+            Console.WriteLine(green.Task8(30, 15));
         }
     }
 }
